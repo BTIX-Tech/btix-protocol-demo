d73e85b696d9f93e40520c1ff0e1762f395fc07a
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-07-17 01:36:20 -0300

    feat: display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const { getEvents, checkNewEvents, getTickets } = require('./events');
-const { checkNewTickets } = require('./tickets');
+const { getEvents, checkNewEvents, getTickets } = require("./events");
+const { checkNewTickets } = require("./tickets");
+
+/**
+ * Display header information for the BTIX Protocol Demo.
+ */
+const displayHeader = () => {
+  console.clear()
+  console.log("=======================================");
+  console.log("");
+  console.log(" BTIX PROTOCOL DEMO");
+  console.log(
+    " This project is a demo showcasing the use of the Protocol API."
+  );
+  console.log("");
+  console.log("=======================================");
+  console.log("");
+};
+
+// Call the displayHeader function to show the header
+displayHeader();
 
 /**
  * Main function to run the synchronization process.
@@ -14,7 +33,7 @@ const run = async () => {
     const tickets = await Promise.all(ticketsPromises);
     await checkNewTickets(tickets.flat());
 
-    console.log('Process completed successfully.');
+    console.log("Process completed successfully.");
   } catch (error) {
     console.error(`Error in run: ${error.message}`);
   }
